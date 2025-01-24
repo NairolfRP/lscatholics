76ae76d8db9dd810a41e6c01a8808e2ad2189acc
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-01-24 16:54:12 +0100

    feat(db): add specific config for prod

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,36 @@
+import env from "#start/env";
 import app from "@adonisjs/core/services/app";
 import { defineConfig } from "@adonisjs/lucid";
 
-const dbConfig = defineConfig({
+const migrations = {
+    naturalSort: true,
+    paths: ["database/migrations"],
+};
+
+const prod = defineConfig({
+    connection: "postgres",
+    connections: {
+        postgres: {
+            client: "pg",
+            connection: {
+                host: env.get("DB_HOST"),
+                port: env.get("DB_PORT"),
+                user: env.get("DB_USER"),
+                password: env.get("DB_PASSWORD"),
+                database: env.get("DB_DATABASE"),
+                ssl: {
+                    rejectUnauthorized: true,
+                },
+            },
+            migrations: {
+                naturalSort: true,
+                paths: ["database/migrations"],
+            },
+        },
+    },
+});
+
+const dev = defineConfig({
     connection: "sqlite",
     connections: {
         sqlite: {
@@ -10,12 +39,11 @@ const dbConfig = defineConfig({
                 filename: app.tmpPath("db.sqlite3"),
             },
             useNullAsDefault: true,
-            migrations: {
-                naturalSort: true,
-                paths: ["database/migrations"],
-            },
+            migrations,
         },
     },
 });
+
+const dbConfig = env.get("NODE_ENV") === "production" ? prod : dev;
 
 export default dbConfig;
