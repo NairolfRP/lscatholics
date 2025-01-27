a2ebfe704d22353692065bb5db2da30ea6405a01
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-01-27 18:44:54 +0100

    chore(lint): add react and react-hooks

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
 import { configApp } from "@adonisjs/eslint-config";
 import eslintConfigPrettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default configApp({
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat["jsx-runtime"],
+    ...reactHooksPlugin.configs["recommended-latest"],
     rules: {
         "prettier/prettier": ["error", { endOfLine: "auto" }, { usePrettierrc: true }],
         "no-restricted-imports": [
