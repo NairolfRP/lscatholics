4568275744d02c176bbba39d324a3ff88e3e7e14
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-01-28 17:47:47 +0100

    fix(shared:str-helper): rename 'limit' parameter to 'maxLength' to avoid name shadowing

--- shared/helpers/str.ts
+++ shared/helpers/str.ts
@@ -1,6 +1,6 @@
 export namespace Str {
-    export function limit(string: string, limit: number = 100, end: string = "..."): string {
-        return string.length <= limit ? string : string.substring(0, limit) + end;
+    export function limit(string: string, maxLength: number = 100, end: string = "..."): string {
+        return string.length <= maxLength ? string : string.substring(0, maxLength) + end;
     }
 
     export function slug(title: string, separator: string = "-") {
