a5c8642e90206b7e352fe9337ca961ac743b84e9
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-01-31 07:13:30 +0100

    fix(config:database): require ssl to production

--- config/database.ts
+++ config/database.ts
@@ -12,6 +12,12 @@ const dbConfig = defineConfig({
                 user: env.get("DB_USER"),
                 password: env.get("DB_PASSWORD"),
                 database: env.get("DB_DATABASE"),
+                ssl:
+                    env.get("NODE_ENV") === "production"
+                        ? {
+                              rejectUnauthorized: env.get("NODE_ENV") === "production",
+                          }
+                        : false,
             },
             migrations: {
                 naturalSort: true,
