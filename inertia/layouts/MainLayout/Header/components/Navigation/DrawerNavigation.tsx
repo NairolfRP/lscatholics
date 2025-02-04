df2dd5742a43d4ab0905b55c18d08582fe9502af
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-02-04 20:58:01 +0100

    feat(DrawerNavigation): add collapsible submenu & update drawer UI
    - Introduced a new ListItemButtonWithCollapse component to handle nested submenu items with expand/collapse behavior.
    - Integrated Material UI's Collapse, ExpandLess, and ExpandMore for a better navigation experience.
    - Added AppLogo as a subheader and a CloseIcon button for improved drawer control.
    - Refactored and removed unused imports for cleaner code.

--- inertia/layouts/MainLayout/Header/components/Navigation/DrawerNavigation.tsx
+++ inertia/layouts/MainLayout/Header/components/Navigation/DrawerNavigation.tsx
@@ -3,26 +3,77 @@ import { useTranslation } from "@/hooks/use_translation";
 import { menuItems } from "@/layouts/MainLayout/Header/components/Navigation/config/menu_items";
 import { Link } from "@inertiajs/react";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Divider, Drawer, ListItemButton, ListItemText } from "@mui/material";
+import { Drawer, ListItemButton, ListItemText } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import Typography from "@mui/material/Typography";
-import * as React from "react";
 import { useState } from "react";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import type { HeaderNavSubmenuItem } from "@/features/navigation/types/navigation";
+import AppLogo from "@/layouts/MainLayout/Header/components/AppLogo/AppLogo";
+import Box from "@mui/material/Box";
+import CloseIcon from "@mui/icons-material/Close";
+
+function ListItemButtonWithCollapse({
+    label,
+    items,
+}: {
+    label: string;
+    items: HeaderNavSubmenuItem[];
+}) {
+    const { t } = useTranslation();
+    const [open, setOpen] = useState(false);
+
+    const toggleOpen = useEventCallback(() => setOpen((prev) => !prev));
+
+    return (
+        <>
+            <ListItemButton onClick={toggleOpen} sx={{ maxWidth: "85%" }}>
+                <ListItemText primary={t(label)} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit sx={{ bgcolor: "#035d96" }}>
+                <List component="div" disablePadding>
+                    {items.map((item, itemKey) => (
+                        <>
+                            <ListItemButton
+                                key={itemKey}
+                                component={Link}
+                                sx={{ pl: 4 }}
+                                href={item.link || "#"}
+                            >
+                                <ListItemText primary={t(item.name)} />
+                            </ListItemButton>
+                            {item.items &&
+                                item.items.map((subItem, subItemKey) => (
+                                    <ListItemButton
+                                        key={subItemKey}
+                                        component={Link}
+                                        sx={{ pl: 8 }}
+                                        href={subItem.link ? subItem.link : "#"}
+                                    >
+                                        <ListItemText primary={t(subItem.name)} />
+                                    </ListItemButton>
+                                ))}
+                        </>
+                    ))}
+                </List>
+            </Collapse>
+        </>
+    );
+}
 
 export default function DrawerNavigation() {
     const { t } = useTranslation();
-
     const [open, setOpen] = useState(false);
-
-    const handleClick = useEventCallback(() => setOpen((prev) => !prev));
-    const handleClose = useEventCallback(() => setOpen(false));
+    const toggleDrawer = useEventCallback(() => setOpen((prev) => !prev));
 
     return (
         <>
             <IconButton
-                onClick={handleClick}
+                onClick={toggleDrawer}
                 size="large"
                 color="inherit"
                 aria-label="menu"
@@ -37,50 +88,46 @@ export default function DrawerNavigation() {
             </IconButton>
             <Drawer
                 open={open}
-                onClose={handleClose}
+                onClose={toggleDrawer}
                 PaperProps={{
                     sx: {
+                        width: "100%",
                         backgroundColor: "primary.main",
                         color: "primary.contrastText",
                         overflow: "auto",
                     },
                 }}
             >
-                <List>
-                    {menuItems.map((menu, index) => {
-                        return (
-                            <React.Fragment key={index}>
-                                {!menu.items ? (
-                                    <ListItem disablePadding>
-                                        <ListItemButton
-                                            component={Link}
-                                            href={menu.link ? menu.link : "#"}
-                                        >
-                                            <ListItemText primary={t(menu.name)} />
-                                        </ListItemButton>
-                                    </ListItem>
-                                ) : (
-                                    <>
-                                        <ListItem>
-                                            <Typography variant="h5">{t(menu.name)}</Typography>
-                                            <Divider />
-                                        </ListItem>
-                                    </>
-                                )}
-                                {menu.items &&
-                                    menu.items.map((subMenuItem, subIndex) => (
-                                        <ListItem disablePadding key={subIndex}>
-                                            <ListItemButton
-                                                component={Link}
-                                                href={subMenuItem.link ? subMenuItem.link : "#"}
-                                            >
-                                                <ListItemText primary={t(subMenuItem.name)} />
-                                            </ListItemButton>
-                                        </ListItem>
-                                    ))}
-                            </React.Fragment>
-                        );
-                    })}
+                <List subheader={<AppLogo />}>
+                    <Box
+                        onClick={toggleDrawer}
+                        sx={(theme) => ({
+                            position: "fixed",
+                            top: 20,
+                            right: 20,
+                            display: "flex",
+                            alignItems: "center",
+                            padding: theme.spacing(0, 1),
+                            ...theme.mixins.toolbar,
+                        })}
+                    >
+                        <CloseIcon fontSize="large" />
+                    </Box>
+                    {menuItems.map((menu, index) =>
+                        !menu.items ? (
+                            <ListItem key={index} disablePadding>
+                                <ListItemButton component={Link} href={menu.link || "#"}>
+                                    <ListItemText primary={t(menu.name)} />
+                                </ListItemButton>
+                            </ListItem>
+                        ) : (
+                            <ListItemButtonWithCollapse
+                                key={index}
+                                label={menu.name}
+                                items={menu.items}
+                            />
+                        ),
+                    )}
                 </List>
             </Drawer>
         </>
