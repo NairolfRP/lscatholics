648642b9abc7bfc0541e4413892f22f87b3d6ad6
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-02-05 03:25:32 +0100

    refactor(ui:ScrollToTop): replace custom scroll listener with useScrollTrigger
    - Removed custom useEventCallback and useEventListener hooks.
    - Leveraged Material UI's useScrollTrigger for scroll detection.
    - Simplified logic for improved performance and readability.

--- inertia/components/ScrollToTop.tsx
+++ inertia/components/ScrollToTop.tsx
@@ -1,22 +1,19 @@
-import { useEventCallback } from "@/hooks/use_event_callback";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import Zoom from "@mui/material/Zoom";
 import Fab from "@mui/material/Fab";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import { useEventListener } from "@/hooks/use_event_listener";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 
 export default function ScrollToTop() {
-    const [visible, setVisible] = useState(false);
-
-    const onScroll = useEventCallback(() => {
-        setVisible(window.scrollY > 300);
+    const visible = useScrollTrigger({
+        target: window || undefined,
+        disableHysteresis: true,
+        threshold: 300,
     });
 
     const handleClick = useCallback(() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
     }, []);
-
-    useEventListener("scroll", onScroll);
 
     return (
         <Zoom in={visible}>
