66161e37c1e7f7e19e46225f75e43a0345cd9402
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-02-06 03:49:37 +0100

    feat(departments): use default image asset for missing image prop
    - Imported DefaultImage from assets.
    - Replaced hard-coded fallback URL with DefaultImage.

--- inertia/features/departments/components/StaffListLeader/StaffListLeader.tsx
+++ inertia/features/departments/components/StaffListLeader/StaffListLeader.tsx
@@ -6,6 +6,7 @@ import Grid from "@mui/material/Grid2";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
+import DefaultImage from "@/assets/images/orange-781119_1920.png";
 
 const GridItem = styled(Paper)(({ theme }) => ({
     ...theme.typography.body1,
@@ -21,11 +22,7 @@ export default function StaffListLeader({ name, position, image }: StaffListLead
                     <GridItem elevation={0}>
                         <Box
                             component="img"
-                            src={
-                                image
-                                    ? image
-                                    : "https://lacatholics.org/wp-content/uploads/2018/11/shutterstock_324535487-scaled.jpg"
-                            }
+                            src={image ? image : DefaultImage}
                             alt=""
                             sx={{ width: "auto", height: "auto", maxWidth: "100%" }}
                         />
