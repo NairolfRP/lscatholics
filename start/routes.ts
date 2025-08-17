14c3ae818f96b0c1dc956a0e10ea7a36e7384e21
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-08-17 03:21:05 +0200

    fix: change endpoint fleeca/callback/:token to api/payment/fleeca/callback/:token

--- start/routes.ts
+++ start/routes.ts
@@ -141,12 +141,12 @@ router
     .group(() => {
         router.post("/cancel", [PaymentController, "cancel"]);
 
-        /*router
+        router
             .get("/fleeca/callback/:token", [PaymentController, "callback"])
-            .where("token", /^[a-zA-Z0-9-_]{30,}$/);*/
+            .where("token", /^[a-zA-Z0-9-_]{30,}$/);
     })
     .prefix("/api/payment");
 
-router
+/*router
     .get("/fleeca/callback/:token", [PaymentController, "callback"])
-    .where("token", /^[a-zA-Z0-9-_]{30,}$/);
+    .where("token", /^[a-zA-Z0-9-_]{30,}$/);*/
