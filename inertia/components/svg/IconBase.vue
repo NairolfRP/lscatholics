d0b293b2cb8c7c1181b6e237942bf655f2d0baf2
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-08-24 12:08:01 +0200

    feat: add custom icons

---
+++ inertia/components/svg/IconBase.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :viewBox="viewBox"
+    :width="width"
+    :height="height"
+    :aria-labelledby="iconName"
+    role="img"
+    v-bind="$attrs"
+    @click="handleClick"
+  >
+    <title :id="iconName" lang="en">{{ iconName }} icon</title>
+    <g :style="{ fill: iconColor }">
+      <slot />
+    </g>
+  </svg>
+</template>
+<script lang="ts" setup>
+import { defaultIconProps, type IconProps } from '@/composables/use_icon_props'
+
+withDefaults(defineProps<IconProps>(), defaultIconProps)
+
+const emit = defineEmits<{
+  click: [e: MouseEvent]
+}>()
+
+const handleClick = (e: MouseEvent) => {
+  if (props.clickable) {
+    emit('click', e)
+  }
+}
+</script>
---
+++ inertia/composables/use_icon_props.ts
@@ -0,0 +1,30 @@
+import type { CSSProperties } from 'vue'
+
+export type IconProps = {
+  iconName?: string
+  viewBox?: string
+  width?: number | string
+  height?: number | string
+  iconColor?: CSSProperties['color']
+}
+
+export const defaultIconProps: Partial<IconProps> = {
+  iconName: 'box',
+  viewBox: '0 0 24 24',
+  width: 18,
+  height: 18,
+  iconColor: 'currentColor',
+}
+
+export const useIconEvents = () => {
+  return {
+    click: [] as ((e: MouseEvent) => void)[],
+  }
+}
+
+export function createIconProps(customDefaults: Parital<IconProps> = {}) {
+  return {
+    ...defaultIconProps,
+    ...customDefaults,
+  }
+}
