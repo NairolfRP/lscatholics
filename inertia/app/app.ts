73ce5f0870e82154a97c5ec0e274dc4fe9915972
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-08-24 12:05:45 +0200

    feat: add tuyau

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -1,11 +1,13 @@
-/// <reference path="./adonisrc.ts" />
-/// <reference path="./config/inertia.ts" />
+/// <reference path="../../adonisrc.ts" />
+/// <reference path="../../config/inertia.ts" />
 
 import 'inertia/css/app.css'
 import type { DefineComponent } from 'vue'
 import { createSSRApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+import { TuyauPlugin } from '@tuyau/inertia/vue'
+import { tuyau } from '@/lib/tuyau'
 
 const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
 
@@ -24,6 +26,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createSSRApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(TuyauPlugin, { client: tuyau })
       .mount(el)
   },
 })
--- inertia/app/ssr.ts
+++ inertia/app/ssr.ts
@@ -1,6 +1,8 @@
 import { createInertiaApp } from '@inertiajs/vue3'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h, type DefineComponent } from 'vue'
+import { TuyauPlugin } from '@tuyau/inertia/vue'
+import { tuyau } from '@/lib/tuyau'
 
 export default function render(page: any) {
   return createInertiaApp({
@@ -12,7 +14,9 @@ export default function render(page: any) {
     },
 
     setup({ App, props, plugin }) {
-      return createSSRApp({ render: () => h(App, props) }).use(plugin)
+      return createSSRApp({ render: () => h(App, props) })
+        .use(plugin)
+        .use(TuyauPlugin, { client: tuyau })
     },
   })
 }
