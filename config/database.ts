aa28eb09eb6e16d5645ca25076a276d9afae9a57
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-08-26 12:50:07 +0200

    chore: enable database debug in dev

--- config/database.ts
+++ config/database.ts
@@ -1,5 +1,6 @@
 import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
+import app from '@adonisjs/core/services/app'
 
 const dbConfig = defineConfig({
   connection: 'postgres',
@@ -17,6 +18,7 @@ const dbConfig = defineConfig({
         naturalSort: true,
         paths: ['database/migrations'],
       },
+      debug: app.inDev,
     },
   },
 })
