9599a53e0d4279d4c87d26bb44aa22a9599dc005
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-08-27 16:40:24 +0200

    chore(types): add shims_vue.d.ts

---
+++ inertia/shims_vue.d.ts
@@ -0,0 +1,21 @@
+declare module '*.vue'
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  const src: string
+  export default src
+}
