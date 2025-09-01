a0429469f7947be6a655ce8ffa6dc987b8e0e546
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-09-01 13:52:29 +0200

    fix(auth): make login button redirect to sign-in route

--- inertia/components/auth/LoginButton.vue
+++ inertia/components/auth/LoginButton.vue
@@ -1,15 +1,18 @@
 <template>
-  <Button
-    variant="ghost"
-    aria-label="Se connecter"
-    class="uppercase text-sm lg:text-xs xl:text-base"
-  >
-    <LogIn class="size-6 lg:size-4 xl:size-5" />
-    <span class="lg:hidden laptop:block">Connexion</span>
-  </Button>
+  <a :href="tuyau.$url('signIn')">
+    <Button
+      variant="ghost"
+      aria-label="Se connecter"
+      class="uppercase text-sm lg:text-xs xl:text-base"
+    >
+      <LogIn class="size-6 lg:size-4 xl:size-5" />
+      <span class="lg:hidden laptop:block">Connexion</span>
+    </Button>
+  </a>
 </template>
 
 <script lang="ts" setup>
 import { Button } from '@/components/ui/button'
 import { LogIn } from 'lucide-vue-next'
+import { tuyau } from '@/lib/tuyau'
 </script>
