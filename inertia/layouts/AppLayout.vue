61e77fa64af362737cef76041565751766fdcc0e
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-09-04 16:13:20 +0200

    feat: add favicon

--- inertia/layouts/AppLayout.vue
+++ inertia/layouts/AppLayout.vue
@@ -1,4 +1,11 @@
 <template>
+  <Head>
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+  </Head>
   <div class="flex flex-col min-h-screen bg-background font-sans antialiased">
     <AppHeader />
 
@@ -19,6 +26,7 @@ import 'vue-sonner/style.css'
 import AppHeader from '@/components/layout/AppHeader.vue'
 import AppFooter from '@/components/layout/AppFooter.vue'
 import ScrollToTopButton from '@/components/ScrollToTopButton.vue'
+import { Head } from '@inertiajs/vue3'
 
 interface Props {
   title?: string
