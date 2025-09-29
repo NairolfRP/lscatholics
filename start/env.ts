1a329cfced7373ef9689b08005ac74ee5511bdf7
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-09-29 21:38:10 +0200

    fix(env): set host, NODE_ENV and port as optional

--- start/env.ts
+++ start/env.ts
@@ -12,10 +12,10 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
+  NODE_ENV: Env.schema.enum.optional(['development', 'production', 'test'] as const),
+  PORT: Env.schema.number.optional(),
   APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
+  HOST: Env.schema.string.optional({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
 
   /*
