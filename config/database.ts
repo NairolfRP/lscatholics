8f4f204a4ac19719cd22edab91d37f32155f7016
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-09-29 20:50:07 +0200

    fix(config): enable database ssl in production

--- config/database.ts
+++ config/database.ts
@@ -14,6 +14,7 @@ const dbConfig = defineConfig({
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
+        ssl: app.inProduction,
       },
       migrations: {
         naturalSort: true,
