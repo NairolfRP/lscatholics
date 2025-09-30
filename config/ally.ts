6608d0a4d7e3564f32e1ff979420bf926bc193d5
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2025-09-30 09:20:45 +0200

    fix: gtaw oauth callback

--- config/ally.ts
+++ config/ally.ts
@@ -8,7 +8,7 @@ const allyConfig = defineConfig({
     server: 'fr',
     clientId: env.get('GTAW_CLIENT_ID'),
     clientSecret: env.get('GTAW_CLIENT_SECRET'),
-    callbackUrl: 'https://archls.infos.st/api/auth/gtaw/callback',
+    callbackUrl: 'https://archls.infos.st/api/auth/callback/gtaw',
   }),
   discord: services.discord({
     clientId: env.get('DISCORD_CLIENT_ID'),
