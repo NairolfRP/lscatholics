1118347c3516a5e274a4ca9a77bc0b15217af2f4
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-01-16 17:43:06 +0100

    Revert "build: replace sqlite3 with better-sqlite3 for test environment"
    This reverts commit 54288adeb539f08279283c0f730be94ac7cdb0f2.
    
    # Conflicts:
    #	.github/workflows/test.yaml.yml

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,6 @@
 import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 import app from '@adonisjs/core/services/app'
-import type { DatabaseConfig, LibSQLConfig } from '@adonisjs/lucid/types/database'
 
 const tursoUrl = env.get('TURSO_DATABASE_URL')
 const tursoToken = env.get('TURSO_AUTH_TOKEN')
@@ -22,68 +21,54 @@ const getTursoConnectionString = () => {
   return tursoUrl
 }
 
-const sharedConnectionConfig: Partial<LibSQLConfig> = {
-  pool: {
-    afterCreate: (conn: any, done: any) => {
-      conn.run('PRAGMA busy_timeout = 5000;')
-      done()
-    },
-    min: 1,
-    max: 10,
-    acquireTimeoutMillis: 30000,
-    createTimeoutMillis: 30000,
-    idleTimeoutMillis: 10000,
-    reapIntervalMillis: 1000,
-    createRetryIntervalMillis: 200,
-    propagateCreateError: true,
-  },
-  migrations: {
-    naturalSort: true,
-    paths: ['database/migrations'],
-  },
-  useNullAsDefault: true,
-}
-
-const connections: DatabaseConfig['connections'] = {
-  sqlite: {
-    client: 'libsql',
-    connection: {
-      filename: getTursoConnectionString(),
-      debug: app.inDev,
-    },
-    ...sharedConnectionConfig,
-    debug: app.inDev,
-  },
-  tests: {
-    client: 'better-sqlite3',
-    connection: {
-      filename: './tmp/test.db',
-    },
-    ...sharedConnectionConfig,
-    debug: true,
-  },
-  /*postgres: {
-    client: 'pg',
-    connection: {
-      host: env.get('DB_HOST'),
-      port: env.get('DB_PORT'),
-      user: env.get('DB_USER'),
-      password: env.get('DB_PASSWORD'),
-      database: env.get('DB_DATABASE'),
-      ssl: app.inProduction,
-    },
-    migrations: {
-      naturalSort: true,
-      paths: ['database/migrations'],
-    },
-    debug: app.inDev,
-  },*/
-}
-
 const dbConfig = defineConfig({
   prettyPrintDebugQueries: app.inDev,
-  connection: app.inTest ? 'tests' : 'sqlite',
-  connections,
+  connection: 'sqlite',
+  connections: {
+    sqlite: {
+      client: 'libsql',
+      connection: {
+        filename: getTursoConnectionString(),
+        debug: app.inDev,
+      },
+      pool: {
+        afterCreate: (conn: any, done: any) => {
+          conn.run('PRAGMA busy_timeout = 5000;')
+          done()
+        },
+        min: 1,
+        max: 10,
+        acquireTimeoutMillis: 30000,
+        createTimeoutMillis: 30000,
+        idleTimeoutMillis: 10000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 200,
+        propagateCreateError: true,
+      },
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+      useNullAsDefault: true,
+      debug: app.inDev,
+    },
+    postgres: {
+      client: 'pg',
+      connection: {
+        host: env.get('DB_HOST'),
+        port: env.get('DB_PORT'),
+        user: env.get('DB_USER'),
+        password: env.get('DB_PASSWORD'),
+        database: env.get('DB_DATABASE'),
+        ssl: app.inProduction,
+      },
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+      debug: app.inDev,
+    },
+  },
 })
 
 export default dbConfig
