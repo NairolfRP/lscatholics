fbd5dd22e4d5ebcd2acac559b02c154fecbc648d
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-01-18 21:29:31 +0100

    feat(contact): add headquarters map

--- inertia/features/contact/components/our-location/ContactOurLocation.vue
+++ inertia/features/contact/components/our-location/ContactOurLocation.vue
@@ -4,12 +4,23 @@
       <h2 class="text-3xl font-bold text-catholic-purple mb-8 text-center font-serif">
         Nous trouver
       </h2>
-      <div class="aspect-video bg-gray-300 rounded-lg flex items-center justify-center">
-        <p class="text-gray-600">Carte interactive - Archevêché de Los Santos</p>
-        <p class="text-sm text-gray-500 mt-1">À venir prochainement</p>
-      </div>
+
+      <GTA5Map class="aspect-video rounded-lg w-full" :center="[-685.58, -765.889]" :zoom="5">
+        <MapMarker :position="[-685.58, -765.889]">
+          <div class="p-2">
+            <h4 class="font-bold">Tour de la Cathédrale Notre-Dame-des-Saints</h4>
+            Ginger Street, Little Seoul<br />
+            Los Santos, SA 90010
+          </div>
+        </MapMarker>
+      </GTA5Map>
     </div>
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+const GTA5Map = defineAsyncComponent(() => import('@/shared/components/map/GTA5Map.vue'))
+const MapMarker = defineAsyncComponent(() => import('@/shared/components/map/MapMarker.vue'))
+</script>
