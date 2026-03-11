d756627a3c977f401f9ea6fdab50d5be80098c4e
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-03-11 05:06:12 +0100

    fix: mobile navigation styles

--- inertia/shared/components/layout/default/mobile-navigation.tsx
+++ inertia/shared/components/layout/default/mobile-navigation.tsx
@@ -29,103 +29,99 @@ export default function MobileNavigation() {
   }
 
   return (
-    <>
-      <Sheet open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
-        <SheetTrigger className="lg:hidden py-5 px-10" asChild>
-          <Button variant="ghost" size="icon">
-            <Menu className="size-8" />
-          </Button>
-        </SheetTrigger>
+    <Sheet open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
+      <SheetTrigger className="lg:hidden py-5 px-10" asChild>
+        <Button variant="ghost" size="icon">
+          <Menu className="size-8" />
+        </Button>
+      </SheetTrigger>
 
-        <SheetContent className="bg-primary-500 text-primary-foreground z-100 w-full h-full pb-20 overflow-y-scroll pt-safe pl-safe pr-safe">
-          <SheetHeader>
-            <SheetTitle>
-              <img alt="Logo" src={Logo} className="w-20 h-20" />
-            </SheetTitle>
-            <SheetDescription />
-          </SheetHeader>
+      <SheetContent className="bg-primary-500 text-primary-foreground z-100 w-full h-full pb-20 overflow-y-scroll pt-safe pl-safe pr-safe">
+        <SheetHeader>
+          <SheetTitle>
+            <img alt="Logo" src={Logo} className="w-20 h-20" />
+          </SheetTitle>
+          <SheetDescription />
+        </SheetHeader>
 
-          <ul className="flex flex-col gap-10">
-            <li>
-              <a
-                onClick={() => handleNavigate('home')}
-                className="mobile-navigation-item-title flex gap-2 items-center"
-              >
-                <House /> Accueil
-              </a>
+        <ul className="flex flex-col gap-10">
+          <li>
+            <a
+              onClick={() => handleNavigate('home')}
+              className="mobile-navigation-item-title flex gap-2 items-center cursor-pointer"
+            >
+              <House /> Accueil
+            </a>
+          </li>
 
-              {MENU_ITEMS.map((item) => (
-                <li key={item.id}>
-                  {item.children ? (
-                    <Accordion type="single" collapsible>
-                      <AccordionItem value={`mobile-nav-${item.id}-submenu`}>
-                        <AccordionTrigger className="mobile-navigation-item-title">
-                          {item.label}
-                        </AccordionTrigger>
-                        <AccordionContent className="bg-primary pl-5 flex flex-col gap-5 mt-3 py-4">
-                          {item.children.map((subItem) => {
-                            if (subItem.route) {
-                              return (
-                                <a
-                                  key={subItem.id}
-                                  onClick={() =>
-                                    handleNavigate(subItem.route!, subItem.routeParams)
-                                  }
-                                  className="text-base pl-3 cursor-pointer"
-                                >
-                                  {subItem.label}
-                                </a>
-                              )
-                            }
-                            if (subItem.href) {
-                              return (
-                                <a
-                                  key={subItem.id}
-                                  href={subItem.href}
-                                  target={subItem.target}
-                                  className="text-base pl-3"
-                                >
-                                  {subItem.label}
-                                </a>
-                              )
-                            }
-                            return <span key={subItem.id}>{subItem.label}</span>
-                          })}
-                        </AccordionContent>
-                      </AccordionItem>
-                    </Accordion>
-                  ) : item.route ? (
-                    <a
-                      onClick={() => handleNavigate(item.route!, item.routeParams)}
-                      className="mobile-navigation-item-title block cursor-pointer"
-                    >
+          <Accordion type="single" collapsible className="flex flex-col gap-10">
+            {MENU_ITEMS.map((item) => (
+              <li key={item.id}>
+                {item.children ? (
+                  <AccordionItem value={`mobile-nav-${item.id}-submenu`}>
+                    <AccordionTrigger className="mobile-navigation-item-title">
                       {item.label}
-                    </a>
-                  ) : (
-                    <a
-                      href={item.href}
-                      target={item.target}
-                      className="mobile-navigation-item-title block"
-                    >
-                      {item.label}
-                    </a>
-                  )}
-                </li>
-              ))}
-
-              <li className="px-5">
-                <Button
-                  variant="secondary"
-                  className="mobile-navigation-item-title w-full"
-                  onClick={() => handleNavigate('donate.index')}
-                >
-                  <HandHeart /> Faire un don
-                </Button>
+                    </AccordionTrigger>
+                    <AccordionContent className="bg-primary pl-5 flex flex-col gap-5 mt-3 py-4">
+                      {item.children.map((subItem) => {
+                        if (subItem.route) {
+                          return (
+                            <a
+                              key={subItem.id}
+                              onClick={() => handleNavigate(subItem.route!, subItem.routeParams)}
+                              className="text-base pl-3 cursor-pointer"
+                            >
+                              {subItem.label}
+                            </a>
+                          )
+                        }
+                        if (subItem.href) {
+                          return (
+                            <a
+                              key={subItem.id}
+                              href={subItem.href}
+                              target={subItem.target}
+                              className="text-base pl-3"
+                            >
+                              {subItem.label}
+                            </a>
+                          )
+                        }
+                        return <span key={subItem.id}>{subItem.label}</span>
+                      })}
+                    </AccordionContent>
+                  </AccordionItem>
+                ) : item.route ? (
+                  <a
+                    onClick={() => handleNavigate(item.route!, item.routeParams)}
+                    className="mobile-navigation-item-title block cursor-pointer"
+                  >
+                    {item.label}
+                  </a>
+                ) : (
+                  <a
+                    href={item.href}
+                    target={item.target}
+                    className="mobile-navigation-item-title block"
+                  >
+                    {item.label}
+                  </a>
+                )}
               </li>
-            </li>
-          </ul>
-        </SheetContent>
-      </Sheet>
-    </>
+            ))}
+          </Accordion>
+
+          <li className="px-5">
+            <Button
+              variant="secondary"
+              className="mobile-navigation-item-title w-full"
+              onClick={() => handleNavigate('donate.index')}
+            >
+              <HandHeart /> Faire un don
+            </Button>
+          </li>
+        </ul>
+      </SheetContent>
+    </Sheet>
   )
 }
