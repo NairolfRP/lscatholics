ca7f1b162486902bfe9de189d1179f5731d61b61
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-03-14 14:32:14 +0100

    fix: ssr bundle path

--- config/inertia.ts
+++ config/inertia.ts
@@ -4,6 +4,7 @@ const inertiaConfig = defineConfig({
   ssr: {
     enabled: true,
     entrypoint: 'inertia/ssr.tsx',
+    bundle: 'build/ssr/ssr.js',
     pages: (_ctx, page) => !page.startsWith('dashboard') && !page.startsWith('profile'),
   },
 })
