87089c504e3906da0567f39f1c43a4609418df58
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-07-24 21:33:17 +0200

    refactor(form): enhance InputField to support dynamic input types and handle number conversion

--- src/shared/components/form/input-field.tsx
+++ src/shared/components/form/input-field.tsx
@@ -1,21 +1,33 @@
-import type { ReactNode } from 'react'
+import type { HTMLInputTypeAttribute, ReactNode } from 'react'
 import { useId } from 'react'
 import { Input } from '#/shared/components/ui/input'
 import { useFieldContext } from '#/shared/integrations/form/form-hook'
 import type { FieldComponentProps } from '#/shared/lib/types/form'
 import { Field, FieldDescription, FieldError, FieldLabel } from '../ui/field'
 
-type InputFieldProps = FieldComponentProps<
+type ValueType<T extends HTMLInputTypeAttribute | undefined> = T extends 'number'
+  ? number | undefined
+  : string
+
+type InputFieldProps<TType extends HTMLInputTypeAttribute | undefined> = FieldComponentProps<
   typeof Input,
   {
     label: ReactNode
     description?: string
-  }
+    type?: TType
+  },
+  'type'
 >
 
-export function InputField({ label, description, required, ...props }: InputFieldProps) {
+export function InputField<TType extends HTMLInputTypeAttribute | undefined = undefined>({
+  type,
+  label,
+  description,
+  required,
+  ...props
+}: InputFieldProps<TType>) {
   const generatedId = useId()
-  const field = useFieldContext<string>()
+  const field = useFieldContext<ValueType<TType>>()
 
   const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
 
@@ -27,11 +39,19 @@ export function InputField({ label, description, required, ...props }: InputFiel
         {label}
       </FieldLabel>
       <Input
+        type={type}
         id={fieldId}
         name={field.name}
         value={field.state.value}
         onBlur={field.handleBlur}
-        onChange={(e) => field.handleChange(e.target.value)}
+        onChange={(e) => {
+          const val = e.target.value
+          if (type === 'number') {
+            const numVal = !val || val.trim() === '' ? null : Number(val)
+            return field.handleChange(numVal as any)
+          }
+          field.handleChange(val as any)
+        }}
         required={required}
         aria-required={required}
         aria-invalid={isInvalid}
