8335ebbc697cb5ddffe81e97c5a47cbe94299241
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-08-10 01:42:05 +0200

    feat: improve not found component

--- src/shared/components/ui/fallbacks/default-not-found.tsx
+++ src/shared/components/ui/fallbacks/default-not-found.tsx
@@ -1,8 +1,27 @@
+import { Image } from '@unpic/react'
+
 export function DefaultNotFound() {
   return (
-    <div className="my-60 text-center">
-      <h1 className="mb-4 font-serif text-4xl font-bold md:text-5xl">Page introuvable</h1>
-      <p className="text-xl opacity-90">Cette page n'existe pas ou plus.</p>
+    <div className="mx-auto max-w-7xl container my-48 text-center px-4 flex flex-col items-center">
+      <h1 className="text-3xl md:text-5xl font-bold mb-8">
+        Oops... Page non trouvée
+      </h1>
+
+      <Image
+        src="/assets/images/404.webp"
+        layout="constrained"
+        width={800}
+        height={450}
+        alt="Saint Antoine avec une lanterne et des objets perdus"
+        fetchPriority="high"
+        className="rounded-xl shadow-lg"
+      />
+
+      <div className="mt-6 bg-card p-4 rounded-lg border border-catholic-gold max-w-md">
+        <p className="italic text-muted-foreground">
+          <strong>Prions.</strong> Glorieux saint Antoine, tu as exercé le divin pouvoir de retrouver ce qui était perdu. Aide-moi à retrouver la grâce de Dieu, et rends-moi dévoué au service de Dieu et de la vertu. Fais-moi retrouver ce que j'ai perdu et montre-moi ainsi la présence de ta bonté.
+        </p>
+      </div>
     </div>
   )
 }
