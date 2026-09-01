6963805fea935ec656ab971fbb02976f34eaa883
NairolfRP <122038899+NairolfRP@users.noreply.github.com>
2026-09-01 13:26:18 +0200

    feat(job-posting): add description to page meta tags

--- src/routes/_app/job/$slug.index.tsx
+++ src/routes/_app/job/$slug.index.tsx
@@ -3,6 +3,7 @@ import { envClient } from '#/config/env-client.ts'
 import JobPostingSinglePage from '#/features/job-posting/components/job-posting-single-page.tsx'
 import { singleJobPostingQueryOptions } from '#/features/job-posting/queries.ts'
 import { pageMetadata } from '#/utils/seo.ts'
+import { generateExcerpt } from '#/utils/string.ts'
 
 export const Route = createFileRoute('/_app/job/$slug/')({
   loader: async ({ params: { slug }, context }) => {
@@ -10,12 +11,16 @@ export const Route = createFileRoute('/_app/job/$slug/')({
     return {
       title: data.title,
       slug: data.slug,
+      description: data.description,
     }
   },
   head: ({ loaderData }) => ({
     meta: loaderData
       ? pageMetadata(loaderData.title, {
           metadata: {
+            description: loaderData.description
+              ? generateExcerpt(loaderData.description, 200)
+              : undefined,
             url: `/job/${loaderData.slug}`,
           },
         })
